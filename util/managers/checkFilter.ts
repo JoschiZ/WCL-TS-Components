bba8ca345364f83b38d7b9488358f4534f06d13c
Ellmist <ellimistdev@gmail.com>
2024-04-07 17:24:46 -0700

    Removes managers from template, restores some inadvertantly removed config. fixes example component.

--- util/managers/checkFilter.ts
+++
@@ -1,9 +0,0 @@
-export default <T>(baseObject: T, filters: T[]) => {
-    for (const filter of filters) {
-        for (const key in filter) {
-            if (baseObject[key] !== filter[key]) {
-                return true;
-            }
-        }
-    }
-};
